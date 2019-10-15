04f77e4e8cb74fd634bd3065a799cd49d9b11e80
unknown <paawel97@gmail.com>
2019-10-16 00:46:47 +0200

    powinna działać kamera

--- app/src/main/java/com/example/appka/MainActivity.java
+++ app/src/main/java/com/example/appka/MainActivity.java
@@ -11,45 +11,92 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.SparseArray;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.vision.barcode.Barcode;
+
+import java.util.List;
+
+import info.androidhive.barcode.BarcodeReader;
+
+public class MainActivity extends AppCompatActivity implements BarcodeReader.BarcodeReaderListener{
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private BarcodeReader barcodeReader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // getting barcode instance
+        barcodeReader = (BarcodeReader) getSupportFragmentManager().findFragmentById(R.id.barcode_fragment);
+
+
+        /***
+         * Providing beep sound. The sound file has to be placed in
+         * `assets` folder
+         */
+        // barcodeReader.setBeepSoundFile("shutter.mp3");
+
+        /**
+         * Pausing / resuming barcode reader. This will be useful when you want to
+         * do some foreground user interaction while leaving the barcode
+         * reader in background
+         * */
+        // barcodeReader.pauseScanning();
+        // barcodeReader.resumeScanning();
+    }
+
+    @Override
+    public void onScanned(final Barcode barcode) {
+        Log.e(TAG, "onScanned: " + barcode.displayValue);
+        barcodeReader.playBeep();
+
+        runOnUiThread(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                Toast.makeText(getApplicationContext(), "Barcode: " + barcode.displayValue, Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onScannedMultiple(List<Barcode> barcodes) {
+        Log.e(TAG, "onScannedMultiple: " + barcodes.size());
+
+        String codes = "";
+        for (Barcode barcode : barcodes) {
+            codes += barcode.displayValue + ", ";
+        }
+
+        final String finalCodes = codes;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), "Barcodes: " + finalCodes, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onBitmapScanned(SparseArray<Barcode> sparseArray) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    public void onScanError(String errorMessage) {
+
+    }
+
+    @Override
+    public void onCameraPermissionDenied() {
+        Toast.makeText(getApplicationContext(), "Camera permission denied!", Toast.LENGTH_LONG).show();
+        finish();
     }
 }
