f60ef243ec17e8e69fc09dcee78dbb51a7ac6e27
MichalBogusz <mbogusz998@gmail.com>
2019-10-18 23:09:32 +0200

    Dzialajace skanowanie

--- app/src/main/java/com/example/appka/MainActivity.java
+++ app/src/main/java/com/example/appka/MainActivity.java
@@ -1,42 +1,112 @@
 package com.example.appka;
 
-import android.hardware.Camera;
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.FrameLayout;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    Camera camera;
-    FrameLayout frameLayout;
-    ShowCamera showCamera;
-
-
-
+    private int CAMERA_PERMISSION_CODE = 1;
+    public static TextView tvresult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        frameLayout = (FrameLayout) findViewById(R.id.frameLayout);
 
-        camera = Camera.open();
-        showCamera  = new ShowCamera(this, camera);
+        tvresult = findViewById(R.id.tvresult);
 
+        Button btnScan = findViewById(R.id.btnScan);
+        Button btnCheck = findViewById(R.id.btnCheck);
+        Button btnExit = findViewById(R.id.btnExit);
 
-
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(MainActivity.this, ScanActivity.class);
+                    startActivity(intent);
+                } else {
+                    requestStoragePermission();
+                }
+            }
+        });
+//        btnCheck.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (ContextCompat.checkSelfPermission(MainActivity.this,
+//                        Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+//                    Intent intent = new Intent(MainActivity.this, ScanActivity.class);
+//                    startActivity(intent);
+//                } else {
+//                    requestStoragePermission();
+//                }
+//            }
+//        });
+//        btnExit.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (ContextCompat.checkSelfPermission(MainActivity.this,
+//                        Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+//                    Intent intent = new Intent(MainActivity.this, ScanActivity.class);
+//                    startActivity(intent);
+//                } else {
+//                    requestStoragePermission();
+//                }
+//            }
+//        });
     }
 
+    private void requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
 
+            new AlertDialog.Builder(this)
+                    .setTitle("Wymagane pozwolenie")
+                    .setMessage("Wymagane jest pozwolenie używania kamery")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[] {Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
 
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[] {Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == CAMERA_PERMISSION_CODE)  {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
